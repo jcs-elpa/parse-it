129ee88f09f54a4bf5da0d9b92863396922b60fb
JenChieh <jcs090218@gmail.com>
2020-11-03 19:03:25 +0800

    Complete lisp.

--- parse-it-lisp.el
+++ parse-it-lisp.el
@@ -26,13 +26,40 @@
 
 (require 'parse-it)
 
+(defconst parse-it-lisp--token-type
+  '(("COMMA" . "[,]")
+    ("BACK_QUOTE" . "[`]")
+    ("QT_S" . "[']")
+    ("QT_D" . "[\"]")
+    ("PAREN_OPN" . "[(]")      ; (
+    ("PAREN_CLS" . "[)]"))     ; )
+  "Lisp token type.")
+
+(defconst parse-it-lisp--comment-token-type
+  '(("COMMENT" . "[;]"))     ; ;
+  "Lisp comment token type.")
+
+(defconst parse-it-lisp--into-level-symbols
+  '("PAREN_OPN")
+  "All symbols that goes into one nested level.")
+
+(defconst parse-it-lisp--back-level-symbols
+  '("PAREN_CLS")
+  "All symbols that goes back up one nested level.")
+
+(defun parse-it-lisp--make-token-type ()
+  "Make up the token type."
+  (append parse-it-lisp--token-type
+          parse-it-lisp--comment-token-type
+          parse-it-lex--token-type))
+
 (defun parse-it-lisp (path)
   "Parse the PATH Lisp."
-  (let* ((parse-it-lex--token-type (parse-it-xml--make-token-type))
+  (let* ((parse-it-lex--token-type (parse-it-lisp--make-token-type))
          (token-list (parse-it-lex-tokenize-it path)))
     (parse-it-ast-build token-list
-                        parse-it-xml--into-level-symbols
-                        parse-it-xml--back-level-symbols)))
+                        parse-it-lisp--into-level-symbols
+                        parse-it-lisp--back-level-symbols)))
 
 (provide 'parse-it-lisp)
 ;;; parse-it-lisp.el ends here
