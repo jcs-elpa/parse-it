1ba2b0d52b006734f5c701022cf687f400470419
JenChieh <jcs090218@gmail.com>
2021-01-28 21:54:46 +0800

    Revert

--- test/bootstrap.el
+++ test/bootstrap.el
@@ -28,7 +28,7 @@
       package-user-dir (expand-file-name (make-temp-name "tmp-elpa")
                                          user-emacs-directory))
 
-(let* ((package-archives '(("melpa" . "http://melpa.org/packages/")
+(let* ((package-archives '(("melpa" . "https://melpa.org/packages/")
                            ("gnu" . "http://elpa.gnu.org/packages/")))
        (pkgs '(s)))
   (package-initialize)
