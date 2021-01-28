4b02923b4da328fb33a96af2f575a4dea5d18018
JenChieh <jcs090218@gmail.com>
2021-01-28 21:45:25 +0800

    Update year

--- parse-it-java.el
+++ parse-it-java.el
@@ -1,6 +1,6 @@
 ;;; parse-it-java.el --- Core parser for Java  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019  Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Copyright (C) 2019-2021  Shen, Jen-Chieh <jcs090218@gmail.com>
 
 ;; This file is NOT part of GNU Emacs.
 
